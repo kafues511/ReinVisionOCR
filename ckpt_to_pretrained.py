58b0da6115b6b214a4bd810cfc802f1425b4e3b4
kafues511 <48208448+kafues511@users.noreply.github.com>
2024-06-23 02:57:20 +0900

    設定ファイル更新

--- ckpt_to_pretrained.py
+++ ckpt_to_pretrained.py
@@ -5,8 +5,10 @@ if __name__ == "__main__":
     """チェックポイントから学習済みモデルを作成 (vscode)
     """
     my_app(
-        #r"log_logs\version_0\checkpoints\last.ckpt",
+        #r"log_logs\version_0\checkpoints",
         #r"..\resources\pretrained\mtlmr3m",
         #r"..\resources\pretrained\msgothic001",
+        #r"..\resources\pretrained\SourceHanSans-Bold",
+        #r"..\resources\pretrained\SourceHanSans-Medium",
         "charseg",
     )
--- train.py
+++ train.py
@@ -5,8 +5,38 @@ if __name__ == "__main__":
     """モデルのトレーニング (vscode)
     """
     dataset_directories = [
-        #r"..\resources\datasets\charseg\mtlmr3m\shadow_30\version_0",
-        #r"..\resources\datasets\charseg\mtlmr3m\outline_shadow_30\version_0",
+        #r"..\resources\datasets\charseg\mtlmr3m\haruoto_medium\version_0",
+        #r"..\resources\datasets\charseg\mtlmr3m\karumaruka_extra_medium\version_0",
+        #r"..\resources\datasets\charseg\mtlmr3m\karumaruka_large\version_0",
+        #r"..\resources\datasets\charseg\mtlmr3m\karumaruka_medium\version_0",
+        #r"..\resources\datasets\charseg\mtlmr3m\karumaruka_small\version_0",
+        #r"..\resources\datasets\charseg\mtlmr3m\riddle_joker_medium\version_0",
+        #r"..\resources\datasets\charseg\mtlmr3m\summer_pockets_medium\version_0",
+        #r"..\resources\datasets\charseg\mtlmr3m\tenshi_sz_medium\version_0",
+
+        #r"..\resources\datasets\charseg\msgothic001\dc4_medium\version_0",
+        #r"..\resources\datasets\charseg\msgothic001\erondo02_large\version_0",
+        #r"..\resources\datasets\charseg\msgothic001\erondo02_medium\version_0",
+        #r"..\resources\datasets\charseg\msgothic001\magicha_large\version_0",
+        #r"..\resources\datasets\charseg\msgothic001\magicha_medium\version_0",
+        #r"..\resources\datasets\charseg\msgothic001\pieces_medium\version_0",
+        #r"..\resources\datasets\charseg\msgothic001\selectoblige_large\version_0",
+        #r"..\resources\datasets\charseg\msgothic001\selectoblige_medium\version_0",
+        #r"..\resources\datasets\charseg\msgothic001\selectoblige_small\version_0",
+
+        #r"..\resources\datasets\charseg\SourceHanSans-Bold\otomeki_extra_large\version_0",
+        #r"..\resources\datasets\charseg\SourceHanSans-Bold\otomeki_extra_medium\version_0",
+        #r"..\resources\datasets\charseg\SourceHanSans-Bold\otomeki_extra_small\version_0",
+        #r"..\resources\datasets\charseg\SourceHanSans-Bold\otomeki_large\version_0",
+        #r"..\resources\datasets\charseg\SourceHanSans-Bold\otomeki_medium\version_0",
+        #r"..\resources\datasets\charseg\SourceHanSans-Bold\otomeki_small\version_0",
+
+        #r"..\resources\datasets\charseg\SourceHanSans-Medium\selectoblige_extra_large\version_0",
+        #r"..\resources\datasets\charseg\SourceHanSans-Medium\selectoblige_extra_medium\version_0",
+        #r"..\resources\datasets\charseg\SourceHanSans-Medium\selectoblige_extra_small\version_0",
+        #r"..\resources\datasets\charseg\SourceHanSans-Medium\selectoblige_large\version_0",
+        #r"..\resources\datasets\charseg\SourceHanSans-Medium\selectoblige_medium\version_0",
+        #r"..\resources\datasets\charseg\SourceHanSans-Medium\selectoblige_small\version_0",
     ]
 
     my_app(
